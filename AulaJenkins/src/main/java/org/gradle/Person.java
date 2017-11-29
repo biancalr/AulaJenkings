787d77ad526c0cc276523401f4b0dce4919be4a9
ALUNO <ALUNO@DESKTOP-EL61SJM>
2017-11-29 14:23:50 -0200

    Idade

--- AulaJenkins/src/main/java/org/gradle/Person.java
+++ AulaJenkins/src/main/java/org/gradle/Person.java
@@ -4,13 +4,18 @@ import org.apache.commons.collections.list.GrowthList;
 
 public class Person {
     private final String name;
+	private final int age;
 
-    public Person(String name) {
+    public Person(String name, int age) {
         this.name = name;
+        this.age = age;
         new GrowthList();
     }
 
-    public String getName() {
+	public String getName() {
         return name;
+    }
+    public int getAge() {
+    	return age;
     }
 }
--- AulaJenkins/src/test/java/org/gradle/PersonTest.java
+++ AulaJenkins/src/test/java/org/gradle/PersonTest.java
@@ -6,7 +6,8 @@ import static org.junit.Assert.*;
 public class PersonTest {
     @Test
     public void canConstructAPersonWithAName() {
-        Person person = new Person("Larry");
+        Person person = new Person("Larry", 30);
         assertEquals("Larry", person.getName());
+        assertEquals(30, person.getAge());
     }
 }
